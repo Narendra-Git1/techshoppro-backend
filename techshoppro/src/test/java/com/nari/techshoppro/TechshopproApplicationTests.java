a95b7bfb6b24f1b60286af6699607efb98cf5959
Narendra-Git1 <narendray353@gmail.com>
2026-05-23 13:08:41 +0530

    Initial Spring Boot backend setup

---
+++ techshoppro/src/test/java/com/nari/techshoppro/TechshopproApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nari.techshoppro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechshopproApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
